6e9e160e4528aa858356a5b432262bda8f7da6f4
David Bomba <turbo124@gmail.com>
2020-04-03 11:52:31 +1100

    VuePress

---
+++ .vuepress/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	base: '/invoiceninja.github.io/',
+	title: 'InvoiceNinja in Github Pages',
+	description: 'Static website built for Invoice Ninja Documentation',
+	dest: './docs'
+};
