553dc300df75d9d001fa650d55ba9a154d07e96b
David Bomba <turbo124@gmail.com>
2020-04-03 19:52:29 +1100

    config.js

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    title: 'InvoiceNinja',
+    description: "A to Z documentation for Invoice InvoiceNinja",
+    base: '/', 
+    themeConfig:{
+	    lastUpdated: 'Last Updated', 
+	    smoothScroll: true,
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Github', link: 'http://github.com/invoiceninja/invoiceninja/' },
+        ]
+    }
+};
