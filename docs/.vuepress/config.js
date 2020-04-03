cfc91b302f5ee114728f010cf00de7ccc11997aa
David Bomba <turbo124@gmail.com>
2020-04-03 22:32:39 +1100

    remove versioning plugin

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,4 +2,28 @@ module.exports = {
     title: 'InvoiceNinja',
     description: "A to Z documentation for Invoice InvoiceNinja",
     base: '/', 
+    themeConfig:{
+	    lastUpdated: 'Last Updated', 
+	    smoothScroll: true,
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Github', link: 'http://github.com/invoiceninja/invoiceninja/' },
+        ],
+        sidebar: [
+	      {
+	        title: 'Home',   // required
+	        path: '/',      // optional, which should be a absolute path.
+	        collapsable: false, // optional, defaults to true
+	        sidebarDepth: 1,    // optional, defaults to 1
+	        children: []
+	      },
+	      {
+	        title: 'Introduction',   // required
+	        path: '/introduction/',      // optional, which should be a absolute path.
+	        collapsable: false, // optional, defaults to true
+	        sidebarDepth: 1,    // optional, defaults to 1
+	        children: []
+	      }
+		]
+    }
 };
