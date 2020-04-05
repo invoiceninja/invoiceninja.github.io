fc9ff3ba3f7383997022202ed7a8aefae2efac39
David Bomba <turbo124@gmail.com>
2020-04-05 16:44:41 +1000

    fixes for wrong dir

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -58,7 +58,7 @@ module.exports = {
 	        collapsable: false, // optional, defaults to true
 	        sidebarDepth: 1,    // optional, defaults to 1
 	        children: [
-	        	['/settings/api.md', 'API'],
+	        	['/selfhost/api.md', 'API'],
 	        ]
 	      }
 		]
