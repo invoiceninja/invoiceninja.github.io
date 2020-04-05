cb3ebb6639bc61e4d712a78eb2bf346d3904fac1
David Bomba <turbo124@gmail.com>
2020-04-05 22:42:10 +1000

    fix for bad link

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,7 +8,12 @@ const path = require('path')
 	plugins: [
 			['@vuepress/nprogress'],
 			['@vuepress/back-to-top'],
-			['one-click-copy'],
+			['one-click-copy', {
+			    copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
+			    copyMessage: 'Copy successfully and then paste it for use.', // default is 'Copy successfully and then paste it for use.'
+			    duration: 300, // prompt message display time.
+			    showInMobile: false // whether to display on the mobile side, default: false.
+			}],
 			['versioning', {
 			versionedSourceDir: path.resolve(context.sourceDir, '..', 'versioned_docs'),
 		    pagesSourceDir: path.resolve(context.sourceDir, '..', 'unversioned_pages'),
