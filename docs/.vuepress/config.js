f4dbe38b5dddb93add5ed7bc72e1aac50f124ff8
David Bomba <turbo124@gmail.com>
2020-12-08 07:58:17 +1100

    fixes for sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -39,7 +39,7 @@ const path = require('path')
         sidebar: [
 	      {
 	        title: 'User Guide',   // required
-	        path: '/',      // optional, which should be a absolute path.
+	        path: '/Readme.md',      // optional, which should be a absolute path.
 	        collapsable: true, // optional, defaults to true
 	        sidebarDepth: 1,    // optional, defaults to 1
 	        children: [
