ea14a975cc4572f5ea8712483563df5a64c96426
Benjamin Beganović <k1pstabug@gmail.com>
2021-01-04 16:39:42 +0100

    Add x-next component

---
+++ source/_components/next.blade.php
@@ -0,0 +1,10 @@
+<div class="flex justify-end">
+    <a class="text-blue-500 flex items-center" href="{{ $url }}">
+        <span>{{ $slot }}</span>
+        <svg xmlns="http://www.w3.org/2000/svg" class="p-0 mt-1 h-4" viewBox="0 0 24 24" fill="none"
+             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <polyline points="13 17 18 12 13 7"></polyline>
+            <polyline points="6 17 11 12 6 7"></polyline>
+        </svg>
+    </a>
+</div>
