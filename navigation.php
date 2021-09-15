9015404eeea871f2ed4b3d0036c5dbd5b02d2d8d
David Bomba <turbo124@gmail.com>
2021-09-15 15:13:29 +1000

    Minor fixes

--- navigation.php
+++ navigation.php
@@ -21,7 +21,7 @@ return [
     'Developer guide' => [
         'url' => '/docs/developer-guide',
         'children' => [
-            'Authentication' => '/docs/api/authentication',
+            'Auth' => '/docs/api/authentication',
             'Clients' => '/docs/api/clients',
             'Payment Gateways' => '/docs/payment-gateways'
         ],
