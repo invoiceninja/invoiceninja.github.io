d9eea40fba0779d0bbbf86101dbbb899e7d31704
Hillel Coren <hillelcoren@gmail.com>
2021-11-16 15:55:12 +0200

    Remove proposals

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ return [
     'production' => false,
     'baseUrl' => 'https://invoiceninja.github.io',
     'title' => 'Free Source Available Invoicing, Expenses & Time-Tracking | Invoice Ninja',
-    'description' => 'The leading free source available online invoicing app for freelancers & businesses. Invoice, accept payments, track expenses, create proposals, & time-tasks',
+    'description' => 'The leading free source available online invoicing app for freelancers & businesses. Invoice, accept payments, track expenses, & time-tasks',
     'collections' => [],
     'navigation' => require_once('navigation.php'),
     'isActive' => function ($page, $path) {
