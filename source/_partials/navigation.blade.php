3937acdc8bc776135f8ca82438ca647a1e242e79
Hillel Coren <hillelcoren@gmail.com>
2022-10-20 17:23:35 +0300

    Prevent lastpass in search

--- source/_partials/navigation.blade.php
+++ source/_partials/navigation.blade.php
@@ -8,7 +8,7 @@
         </a>
         <section id="right-side-items" class="text-sm">
             <div class="hidden md:hidden lg:block md:space-x-5">
-                <input type="text" placeholder="Quick search ..." id="topSearchBox"
+                <input type="text" placeholder="Quick search ..." id="topSearchBox" data-lpignore="true"
                        class="pb-1 border-b hover:border-ninja-blue focus:border-ninja-blue focus:outline-none">
                 <a href="/docs/getting-started" class="py-2 border-b border-transparent hover:border-ninja-blue">User Guide</a>
                 <a href="https://app.swaggerhub.com/apis/invoiceninja/invoiceninja" target="_blank"
