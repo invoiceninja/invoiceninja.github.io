f9a225d2e7434a148722e4a6d35aa7a337887d4a
David Bomba <turbo124@gmail.com>
2023-05-01 09:53:41 +1000

    Add translations as span

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -19,17 +19,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;400;500;700&display=swap" rel="stylesheet">
 
-
-      <script type="text/javascript">
-        function googleTranslateElementInit() {
-        new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
-        }
-        </script>
-
-        <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-        
     @yield('before-closing-head')
-
 </head>
 
 @include('_partials.navigation')
@@ -67,6 +57,16 @@
             debug: false
         });
     </script>
+
+
+      <script type="text/javascript">
+        function googleTranslateElementInit() {
+        new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+        }
+        </script>
+
+        <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+        
 
 <style>
 .video {
