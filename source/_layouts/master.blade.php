3ac750ff03ea71c38a7326ad2d7b55138c4ab808
David Bomba <turbo124@gmail.com>
2023-05-01 09:05:11 +1000

    Add translations as span

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -20,6 +20,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;400;500;700&display=swap" rel="stylesheet">
 
     @yield('before-closing-head')
+
         <script type="text/javascript">
         function googleTranslateElementInit() {
             new google.translate.TranslateElement({
