5288f206630e87313e573a78abebf21739c35087
David Bomba <turbo124@gmail.com>
2023-05-01 09:30:11 +1000

    Add translations as span

--- source/_partials/navigation.blade.php
+++ source/_partials/navigation.blade.php
@@ -16,7 +16,6 @@
                 <a target="_blank" href="https://invoicing.co" class="px-5 py-2 border rounded-full hover:border-ninja-blue">Go
                     to app</a>
             </div>
-            <div id="google_translate_element"></div>
             <div class="flex flex-col lg:hidden">
                 <button id="mobile-menu-toggle" class="focus:outline-none">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
@@ -37,6 +36,6 @@
            class="py-2 border-b border-transparent hover:border-ninja-blue">API Documentation</a>
         <a href="https://invoicing.co" target="_blank"
            class="py-2 border-b border-transparent hover:border-ninja-blue">Go to app</a>
-        <span id="google_translate_element"></span>
+        
     </div>
 </nav>
