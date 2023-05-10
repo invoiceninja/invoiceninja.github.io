4af979e3fa40f6e4c322b02d6449d2ca967ce097
David Bomba <turbo124@gmail.com>
2023-05-10 14:54:21 +1000

    StubTesting

---
+++ tests/DocSearchTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class DocSearchTest extends TestCase
+{
+    public function testPushAndPop(): void
+    {
+        $this->assertEquals(
+            1,
+            1
+        );
+    }
+}
